fae1c97436c21bcecfa3e58d5055a3d474394b0c
Eric Kang <97197222+erickdhdock@users.noreply.github.com>
2024-06-20 14:40:30 -0700

    built

--- ros_gazebo_rviz_moveit/06050314/ntest/build/mycobot_description/prefix_override/sitecustomize.py
+++ ros_gazebo_rviz_moveit/06050314/ntest/build/mycobot_description/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/cfh/ntest/install/mycobot_description'
+sys.prefix = sys.exec_prefix = '/Users/changbeankang/Claw_For_Humanity/HOS_II/Hawk-ReSolve/ros_gazebo_rviz_moveit/06050314/ntest/install/mycobot_description'
