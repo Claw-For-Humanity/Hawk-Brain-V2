a510c1512cc4098b6fbeb8ae2412618c4104e56e
Eric Kang <erickdhdock@gmail.com>
2024-07-16 23:02:10 -0700

    Update gz1.launch.py
    working,
    still fake joints and no controllers but successfully spawns robot on both softwares

--- gz_launch_config/gz1.launch.py
+++ gz_launch_config/gz1.launch.py
@@ -46,17 +46,15 @@ def load_yaml(package_name, file_path):
 
 
 # DEMO LAUNCH
-def generate_demo_launch(moveit_config):
+def generate_launch_description():
 
     ######################### Launch Args #########################
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    ld = LaunchDescription()
+    # ld.add_action(DeclareLaunchArgument('use_sim_time', default=True))
+    ld.add_action(DeclareLaunchArgument("publish_frequency", default_value="15.0"))
     desc_shared_path = os.path.join(get_package_share_directory('mycobot_description'))
     moveit_shared_path = os.path.join(get_package_share_directory('mycobot_320pi_moveit2'))
     desc_path = os.path.join(desc_shared_path, 'urdf','mycobot_320_pi_2022','mycobot_320_pi_moveit_2022_gz.urdf')
-    
-    yaml_config = os.path.join(moveit_shared_path,'config', 'ros2_controllers.yaml')
-
-    ld = LaunchDescription()
     
     
     
@@ -81,16 +79,19 @@ def generate_demo_launch(moveit_config):
     ld.add_action(DeclareBooleanLaunchArg("use_rviz", default_value=True))
 
     
-    ######################### COMMANDS #########################
+    ######################### ACTIONS #########################
 
     # ** gazebo **
     ld.add_action( 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [PathJoinSubstitution([FindPackageShare('ros_gz_sim'),
+                [PathJoinSubstitution(
+                    [FindPackageShare('ros_gz_sim'),
                                        'launch',
-                                       'gz_sim.launch.py'])]),
-            launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])])
+                                       'gz_sim.launch.py'])
+                                       ]
+                                       ),
+            launch_arguments=[('gz_args', [' -r -v 4 empty.sdf'])]),
     )
 
     # ** robot_description **
@@ -107,32 +108,37 @@ def generate_demo_launch(moveit_config):
         output='screen',
         arguments=['-topic', 'robot_description', '-name',
                    'arm_control', '-allow_renaming', 'true'],
-                   )
+                   ),
     
     )
 
-
-
-
-
-
-
-    # If there are virtual joints, broadcast static tf by including virtual_joints launch
-    virtual_joints_launch = (
-        moveit_config.package_path / "launch/static_virtual_joint_tfs.launch.py"
+    # ** publish tf2 - for gz **
+    ld.add_action(
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="static_transform_publisher",
+            output="log",
+            arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"],
+            ),
     )
-    if virtual_joints_launch.exists():
-        ld.add_action(
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(str(virtual_joints_launch)),
-            )
-        )
 
-    # Given the published joint states, publish tf for the robot links
+    # ** publish robot state - for gz **
+    ld.add_action(
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name='robot_state_publisher',
+            output="both",
+            parameters=[robot_description],
+        ),
+    )
+
+    # publish tf for the robot links - for rviz
     ld.add_action(
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                str(moveit_config.package_path / "launch/rsp.launch.py")
+                str(f"{moveit_shared_path}/launch/rsp.launch.py")
             ),
         )
     )
@@ -140,59 +146,58 @@ def generate_demo_launch(moveit_config):
     ld.add_action(
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                str(moveit_config.package_path / "launch/move_group.launch.py")
+                str(f"{moveit_shared_path}/launch/move_group.launch.py")
             ),
-        )
+        ),
     )
 
     # Run Rviz and load the default config to see the state of the move_group node
     ld.add_action(
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                str(moveit_config.package_path / "launch/moveit_rviz.launch.py")
+                str(f"{moveit_shared_path}/launch/moveit_rviz.launch.py")
             ),
             condition=IfCondition(LaunchConfiguration("use_rviz")),
-        )
+        ),
     )
 
     # If database loading was enabled, start mongodb as well
     ld.add_action(
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                str(moveit_config.package_path / "launch/warehouse_db.launch.py")
+                str(f"{moveit_shared_path}/launch/warehouse_db.launch.py")
             ),
             condition=IfCondition(LaunchConfiguration("db")),
-        )
+        ),
     )
 
-    # Fake joint driver
+    # Controller Manager -> fake control.
     ld.add_action(
         Node(
             package="controller_manager",
             executable="ros2_control_node",
             parameters=[
-                moveit_config.robot_description,
-                str(moveit_config.package_path / "config/ros2_controllers.yaml"),
+                robot_description,
+                str(f"{moveit_shared_path}/config/ros2_controllers.yaml"),
             ],
-        )
-    )
-    
-    ld.add_action(
-        Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=['-topic', 'robot_description', '-name',
-                   'arm_control', '-allow_renaming', 'true'],
-        )
+        ),
     )
 
-    # gazebo
+    # ** there is no controllers currently so this will be useless **
+    # ld.add_action(
+    #     IncludeLaunchDescription(
+    #         PythonLaunchDescriptionSource(
+    #             str(f"{moveit_shared_path}/launch/spawn_controllers.launch.py")
+    #         ),
+    #     )
+    # )
+
     ld.add_action(
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                str(moveit_config.package_path / "launch/spawn_controllers.launch.py")
-            ),
+        Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        remappings=[('/source_list', '/joint_states')]
         )
     )
 
