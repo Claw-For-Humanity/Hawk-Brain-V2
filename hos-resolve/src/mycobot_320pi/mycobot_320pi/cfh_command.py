a715d342d94d7f75870849612203279cb4857583
Eric Kang <erickdhdock@gmail.com>
2024-12-30 16:24:07 -0800

    Update cfh_command.py
    fixed error causing all output to be 0

--- hos-resolve/src/mycobot_320pi/mycobot_320pi/cfh_command.py
+++ hos-resolve/src/mycobot_320pi/mycobot_320pi/cfh_command.py
@@ -1,3 +1,4 @@
+
 import time
 import math
 import rclpy
@@ -12,25 +13,25 @@ class MyCobotListener(Node):
     def __init__(self):
         super().__init__('listener')
 
-        self.declare_parameter('tcp_ip', '169.254.221.247')  # Replace with Raspberry Pi's IP
+        self.declare_parameter('tcp_ip', '169.254.221.247')  # Raspberry Pi's IP
         self.declare_parameter('tcp_port', 22411)
-        self.declare_parameter('debugging', False)  # New parameter for debugging
-        debugging = self.get_parameter('debugging').get_parameter_value().bool_value
-        
-        if not debugging:
-            print('Attempting to connect to Raspberry Pi')
-            tcp_ip = self.get_parameter('tcp_ip').get_parameter_value().string_value
-            tcp_port = self.get_parameter('tcp_port').get_parameter_value().integer_value
+        self.declare_parameter('debugging', False)
 
+        tcp_ip = self.get_parameter('tcp_ip').get_parameter_value().string_value
+        tcp_port = self.get_parameter('tcp_port').get_parameter_value().integer_value
+        debugging = self.get_parameter('debugging').get_parameter_value().bool_value
+        if debugging:
+            self.client_socket = None
+            print('[LOG]: debugging enabled.\nSkipping connection to rpi')
+
+        else:
+            print('Attempting to connect to Raspberry Pi')
+            
             # ------------------ CONNECTION TO RASPBERRY PI ------------------ 
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client_socket.connect((tcp_ip, tcp_port))
             print(f"Connected to Raspberry Pi @ {tcp_ip}:{tcp_port}")
-        
-        else:
-            print("Debugging mode enabled. Skipping Raspberry Pi connection.")
-            self.client_socket = None  # No socket connection in debugging mode
-
+            
         time.sleep(1)
         
         self.subscription = self.create_subscription(
@@ -41,19 +42,14 @@ class MyCobotListener(Node):
 
 
 
-
     def listener_callback(self, msg):
-
         data_list = []
 
-        if(len(msg.name) != 7):
-            print(f'error has occured at line 49 ')
-
-            print(f'current length is {len(msg.name)}')
-
+        if(len(msg.name) != 6 and len(msg.name) != 7):
+            print('error! length is not 6 or 7\nactual length: {}'.format(len(msg.name)))
             return
         
-
+        
         joint_data = sorted(zip(msg.name, msg.position), key=lambda x: x[0])
         msg.name, msg.position = zip(*joint_data)
         msg.position = list(msg.position)
@@ -62,24 +58,21 @@ class MyCobotListener(Node):
         # print(msg.name)
         print("===="*3)
 
-
         data_list = [round(math.degrees(pos), 2) for pos in msg.position]
         if all(value == 0 for value in data_list):
-            print(msg.position)
             print("All values are 0")
             return
 
         data_list[2] = -data_list[2]
         print(data_list)
 
-        if self.client_socket is None:  # If debugging, skip data sending
-            
-            print("Debugging mode: Processing joint states without sending to Raspberry Pi.")
 
+        if self.client_socket == None:
+            
             return
 
-        else:
-            pass
+
+        ############################### com part ###############################
 
         print("dumping")
         data = pickle.dumps(data_list)
@@ -94,7 +87,11 @@ class MyCobotListener(Node):
         if ack == "ACK":
             print("Acknowledgment received from Raspberry Pi")
 
-        
+
+        # example data is [-7.6, -150.85, 30.91, -71.54, -11.48, 105.88, 0.0]
+        # the following is the coordination of the data
+        # [gripper, j2-1, j3-2, j4-3, j5-4, j6-5, j6o-j6]
+
         print(data_list)
 
 
